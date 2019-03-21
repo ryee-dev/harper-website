6156a0f9ba8ff89c814c56d073c1ef2cb2401ffc
roger yee <ryee.epicodus@gmail.com>
2019-03-21 15:25:52 -0700

    loads proper fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         typekit: {
-          id: 'yfb8knl',
+          id: 'grb0fol',
         },
       },
     },
